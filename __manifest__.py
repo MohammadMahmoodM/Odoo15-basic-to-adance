b9f87a2894b917d046f16fb80ef8781755a6b638
mahmoodlegend <mohammadmahmood441@gmail.com>
2023-06-11 05:43:37 -0400

    default value using context

--- __manifest__.py
+++ __manifest__.py
@@ -27,6 +27,7 @@
         'security/ir.model.access.csv',
         'views/menu.xml',
         'views/patient_view.xml',
+        'views/female_patient_view.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
