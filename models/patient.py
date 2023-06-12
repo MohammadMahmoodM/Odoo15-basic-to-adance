6c8c375d715aed587c29215b5ae39adb7e00d090
mahmoodlegend <mohammadmahmood441@gmail.com>
2023-06-12 03:16:04 -0400

    chatter, field tracking, left side search panel view, search bar, filter, default filter, default search filter, form and tree view everything working and proper making notes in  cherry tree

--- models/patient.py
+++ models/patient.py
@@ -3,11 +3,12 @@ from odoo import fields, models, api
 
 class HospitalPatient(models.Model):  # this is the way of defining model
     _name = "hospital.patient"        # way of defining model name from this name we will use this model
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
 # Now we are going to define some fields
 
-    name = fields.Char(string="Name")
+    name = fields.Char(string="Name", tracking=True)
     ref = fields.Char(string="Reference")  # a unique id for patient
-    age = fields.Integer(string="Age")
+    age = fields.Integer(string="Age", tracking=True)
     gender = fields.Selection([('male','Male'),('female','Female')], string="Gender")
     active = fields.Boolean(string="Active", default=False)
